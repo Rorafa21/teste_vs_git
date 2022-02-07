cd37df1135a8f7e598cbb07b41e0681e7296c095
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 11:48:58 -0300

    concertando /n

--- nome.py
+++ nome.py
@@ -3,5 +3,4 @@ print("Nome")
 nome = input("Digite o seu nome: ")
 idade = input("Digite sua idade: ")
 
-print(f"Seu nome é {nome}, que belo nome você tem em meu chapa! \n
-E vocÊ tem {idade} anos de idade, que bacana em ta novinho.")
+print(f"Seu nome é {nome}, que belo nome você tem em meu chapa! \nE vocÊ tem {idade} anos de idade, que bacana em ta novinho.")
