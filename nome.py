ac79b14a74244526141cdecbb252587e620c0b79
Rodrigo Rafa <93699810+Rorafa21@users.noreply.github.com>
2022-02-07 11:48:25 -0300

    idade

--- nome.py
+++ nome.py
@@ -1,5 +1,7 @@
 print("Nome")
 
 nome = input("Digite o seu nome: ")
+idade = input("Digite sua idade: ")
 
-print(f"Seu nome é {nome}, que belo nome você tem em meu chapa!")
+print(f"Seu nome é {nome}, que belo nome você tem em meu chapa! \n
+E vocÊ tem {idade} anos de idade, que bacana em ta novinho.")
